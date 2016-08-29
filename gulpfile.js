d2cd7b3d5509ab13b640d13dbc08ce2af4e30f93
sga <stephane.gautrin@arrow.com>
2016-08-29 08:05:59 +0200

    Handling amcharts assets

--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,23 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
- mix.sass('app.scss')
-     .scripts([
-      'libs/jquery-2.1.4.js',
-      'libs/bootstrap.js',
-      'libs/sweetalert-dev.js',
-      'libs/lity.js'
-     ], './public/js/libs.js')
-     .styles([
-      'libs/font-awesome.css',
-      'libs/sweetalert.css',
-      'libs/lity.css'
-     ], './public/css/libs.css')
-     .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/**',
-     'public/fonts/bootstrap')
-     .copy('resources/assets/fonts/**',
-     'public/fonts');
+elixir(function (mix) {
+    mix.sass('app.scss')
+        .scripts([
+            'libs/jquery-2.1.4.js',
+            'libs/bootstrap.js',
+            'libs/sweetalert-dev.js',
+            'libs/lity.js'
+        ], './public/js/libs.js')
+        .styles([
+            'libs/font-awesome.css',
+            'libs/sweetalert.css',
+            'libs/lity.css',
+        ], './public/css/libs.css')
+        .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/**',
+        'public/fonts/bootstrap')
+        .copy('resources/assets/amcharts/**',
+        'public/js/amcharts')
+        .copy('resources/assets/fonts/**',
+        'public/fonts');
 });
