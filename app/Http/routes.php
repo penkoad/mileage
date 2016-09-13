ce35da51ce7c922b7561a1177d395caebf278566
sga <stephane.gautrin@arrow.com>
2016-09-13 08:51:08 +0200

    Adding a named route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,7 +16,7 @@ Route::auth();
 Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/', 'HomeController@index');
-    Route::get('/home', 'HomeController@index');
+    Route::get('/home', [ 'as' => 'bills.list', 'uses' => 'HomeController@index']);
     Route::resource('/bills', 'BillController');
     Route::get('/chart', 'ChartController@index');
 });
