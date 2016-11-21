09a8e07209660209b83d3d23a37b377c870be550
sga <stephane.gautrin@arrow.com>
2016-11-21 10:34:58 +0100

    Remove password generation from seeder. Use reset password.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,7 +15,6 @@ class UsersTableSeeder extends Seeder
         \App\User::create([
             'email'    => 'stephane@gautrin.fr',
             'name' => 'Stephane Gautrin',
-            'password' => '$2y$10$lnumBlW9aBA6EQhQdgDdTu6MKyfrMrr68mQ6UHg3BS46cm.bw5L5m'
         ]);
     }
 }
