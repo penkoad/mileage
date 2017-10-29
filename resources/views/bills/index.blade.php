3e17c824624acd08e1b397c0cbd98d833b37385e
Stéphane Gautrin <stephane@gautrin.fr>
2017-10-29 18:51:45 +0100

    Improve layout for bill page

--- resources/views/bills/index.blade.php
+++ resources/views/bills/index.blade.php
@@ -1,8 +1,28 @@
 @extends('layouts.app')
 
+@section('extra_css')
+    <style>
+        body { padding-bottom: 100px; }
+        .level {  display: flex; align-items: center; }
+        .flex { flex: 1; }
+        .mr-1 { margin-right: 1em;}
+        .ml-a { margin-left: auto;}
+        [v-cloak]: { display: none;}
+    </style>
+@endsection
+
 @section('content')
     <div class="container-fluid">
-        <h1>Bills</h1>
+        <div class="level">
+            <div class="flex">
+                <h4>Bills</h4>
+            </div>
+            <button class="btn btn-default">
+                <a href="{{ url('/bills/create') }}" class="">Add a bill</a>
+            </button>
+        </div>
+
+        <br>
 
         <div class="table-responsive">
             <table class="table table-hover">
@@ -20,12 +40,10 @@
                 @endforeach
             </table>
 
-            {{ $bills->render() }}
+            <div class="text-center">
+                {{ $bills->render() }}
+            </div>
         </div>
-        <hr>
-        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-        <a href="{{ url('/bills/create') }}" class="">Add a bill</a>
-
     </div>
 @stop
 
