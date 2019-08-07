2b50591af610eab86159b71183ebddacf237ae3f
Stéphane Gautrin <stephane@gautrin.fr>
2019-08-07 11:04:35 +0200

    Fix a bug in create a new bill form and improve layout

--- resources/views/bills/create.blade.php
+++ resources/views/bills/create.blade.php
@@ -2,58 +2,49 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Create a New Bill</div>
+        <h2>Create a New Bill</h2>
+        <div class="row">
 
-                <div class="panel-body">
-                    <form method="POST" action="{{ route('bills.store') }}" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-                        <div class="row">
-                            <div class='col-sm-12'>
+            <div class="col"></div>
+            <div class="col-8">
+                <form method="POST" action="{{ route('bills.store') }}" enctype="multipart/form-data">
+                    {{ csrf_field() }}
 
-                                @if($errors->has('mileage'))
-                                    <div class="alert alert-danger" role="alert">
-                                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                                        <span class="sr-only">Error:</span>
-                                        {{ $errors->has('mileage') ? $errors->get('mileage')[0] : '' }}
-                                    </div>
-                                @endif
-                                <div class="form-group {{ $errors->has('mileage') ? ' has-error' : '' }}">
-                                    <label for="mileage">Mileage:</label>
-                                    <input type="text" name="mileage" id="mileage" class="form-control" value="{{ old('mileage') }}" required>
-                                </div>
+                    <div class="form-group">
+                        <label for="mileage">Mileage:</label>
+                        <input type="text" name="mileage" id="mileage" class="form-control {{ $errors->has('mileage') ? ' is-invalid' : '' }}" value="{{ old('mileage') }}" required>
+                        <div class="invalid-feedback">
+                            {{ $errors->has('mileage') ? $errors->get('mileage')[0] : '' }}
+                        </div>
+                    </div>
 
-                                @if($errors->has('amount'))
-                                    <div class="alert alert-danger" role="alert">
-                                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                                        <span class="sr-only">Error:</span>
-                                        {{ $errors->has('amount') ? $errors->get('amount')[0] : '' }}
-                                    </div>
-                                @endif
-                                <div class="form-group {{ $errors->has('amount') ? ' has-error' : '' }}">
-                                    <label for="amount">Amount:</label>
-                                    <input type="text" name="amount" id="amount" class="form-control" value="{{ old('amount') }}" required>
-                                </div>
+                    <div class="form-group">
+                        <label for="amount">Amount:</label>
+                        <input type="text" name="amount" id="amount" class="form-control {{ $errors->has('amount') ? ' is-invalid' : '' }}" value="{{ old('amount') }}" required>
+                        <div class="invalid-feedback">
+                            {{ $errors->has('amount') ? $errors->get('amount')[0] : '' }}
+                        </div>
+                    </div>
 
-                                <div class="form-group">
-                                    <label for="added_on">Date:</label>
-
-                                    <div class='input-group date'>
-                                        <input type="text" class="form-control" name="added_on" id="added_on" value="{{ old('added_on') }}">
-                                    </div>
-                                </div>
+                    <div class="form-group">
+                        <label for="added_on">Date:</label>
+                        <div class='input-group date'>
+                            <input type="text" class="form-control {{ $errors->has('added_on') ? ' is-invalid' : '' }}" name="added_on" id="added_on" value="{{ old('added_on') }}">
+                            <div class="invalid-feedback">
+                                {{ $errors->has('added_on') ? $errors->get('added_on')[0] : '' }}
                             </div>
                         </div>
-                        <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
-                            <hr>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary">Create Bill</button>
-                            </div>
+
+                    </div>
+                    <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
+                        <hr>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Create Bill</button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
+            <div class="col"></div>
         </div>
     </div>
 
