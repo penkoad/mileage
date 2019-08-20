dd00aba1bf74e2376b87333fc1cbc7371f9888e2
Stéphane Gautrin <stephane@gautrin.fr>
2019-08-20 16:35:36 +0200

    Keep app.js without defer otherwise jQuery is not loaded in time and keep it at the bottom of html otherwise vue #app is not created. Tricky.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,16 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -30,6 +26,7 @@
     </main>
 </div>
 <!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" ></script>
 <script src="{{ asset('js/libs.js') }}" ></script>
 @yield('bottom_scripts')
 @include('flash')
