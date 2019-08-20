61abfdfcf77f2629c4ff3c2b1edf4b5c22efd462
Stéphane Gautrin <stephane@gautrin.fr>
2019-08-20 13:35:54 +0200

    Improve layout for bills index view

--- resources/views/bills/index.blade.php
+++ resources/views/bills/index.blade.php
@@ -1,50 +1,36 @@
 @extends('layouts.app')
 
-@section('extra_css')
-    <style>
-        body { padding-bottom: 100px; }
-        .level {  display: flex; align-items: center; }
-        .flex { flex: 1; }
-        .mr-1 { margin-right: 1em;}
-        .ml-a { margin-left: auto;}
-        [v-cloak]: { display: none;}
-    </style>
-@endsection
-
 @section('content')
     <div class="container-fluid">
-        <div class="level">
-            <div class="flex">
-                <h4>Bills</h4>
-            </div>
-            <a href="{{ url('/bills/create') }}" class="">
-                <button class="btn btn-default">Add a bill</button>
-            </a>
-        </div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-8">
+                <h4>Bills
+                    <a href="{{ url('/bills/create') }}" class="float-right">
+                        <button type="button" class="btn btn-primary">Add a bill</button>
+                    </a>
+                </h4>
 
-        <br>
-
-        <div class="table-responsive">
-            <table class="table table-hover">
-                <thead class="thead-light">
-                <tr>
-                    <th scope="col">Amount</th>
-                    <th scope="col">Mileage</th>
-                    <th scope="col">Added on</th>
-                </tr>
-                </thead>
-                @foreach($bills as $bill)
+                <table class="table table-hover mt-4">
+                    <thead class="thead-light">
                     <tr>
-                        <td>{{ $bill->amount }}</td>
-                        <td>{{ $bill->mileage }}</td>
-                        <td>{{ date('F d, Y', strtotime($bill->added_on)) }}</td>
+                        <th scope="col">Amount</th>
+                        <th scope="col">Mileage</th>
+                        <th scope="col">Added on</th>
                     </tr>
-                @endforeach
-            </table>
-
-            <div class="text-center">
-                {{ $bills->render() }}
+                    </thead>
+                    @foreach($bills as $bill)
+                        <tr>
+                            <td>{{ $bill->amount }}</td>
+                            <td>{{ $bill->mileage }}</td>
+                            <td>{{ date('F d, Y', strtotime($bill->added_on)) }}</td>
+                        </tr>
+                    @endforeach
+                </table>
+                <div class="float-right">{{ $bills->render() }}</div>
             </div>
+
+            <div class="col"></div>
         </div>
     </div>
 @stop
